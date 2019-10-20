62d74e4f214839929a4dd171e0ce859eb108cce6
DavidGirard117 <56056431+DavidGirard117@users.noreply.github.com>
2019-10-20 10:26:15 -0700

    dg_ch3_ex3

---
+++ src/modulus/Modulus.java
@@ -0,0 +1,36 @@
+package modulus;
+import java.util.Scanner;
+
+/**
+ * Converts centimeters to feet and inches.
+ */
+public class Modulus {
+
+    public static void main(String[] args) {
+        int sec;
+        int hour, min, remainder;
+        final int SEC_PER_MIN = 60;
+        final int MIN_PER_HOUR = 60;
+        Scanner in = new Scanner(System.in);
+        
+        
+        System.out.print("Exactly how many seconds? ");
+        sec = in.nextInt();
+        
+        min = (int) (sec / SEC_PER_MIN);
+        hour = min / MIN_PER_HOUR;
+        remainder = min /MIN_PER_HOUR;
+        System.out.printf("%f sec = %f hour, %f min\n",
+        		sec, hour, remainder);
+        
+        
+        
+
+        
+    }
+
+	
+	}
+
+
+	
